84d2381cda52ee3a52a8649f0b6b151f695c01cc
Bartłomiej Tulejko <Naeris.bt@gmail.com>
2020-06-01 11:56:46 +0200

    ZAD1

--- app/src/main/java/com/example/android_lab04/MainActivity.java
+++ app/src/main/java/com/example/android_lab04/MainActivity.java
@@ -2,14 +2,48 @@ package com.example.android_lab04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<String> target;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO
+
         setContentView(R.layout.activity_main);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+        this.target = new ArrayList<>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,this.target);
+        ListView listView= (ListView)findViewById(R.id.Please);
+        listView.setAdapter(this.adapter);
     }
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 }
